9b5693c7787770322fa73d59884bfb535145c2f3
nayuta <nayutadere@gmail.com>
2026-02-14 21:29:28 +0000

    setup for editing in hitori

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,16 @@
 let
-  user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPo3fNoSawk3dcSpJZYDG2cy+BSzj9YlVubceBt/Lmdb nayuta@shorekeeper";
-  users = [ user1 ];
+  nayuta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPo3fNoSawk3dcSpJZYDG2cy+BSzj9YlVubceBt/Lmdb nayuta@shorekeeper";
 
   shorekeeper = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKLJYyqOYjdYIDFdsVpleCVXl70bRdIqdYiBIORejG68 root@shorekeeper";
+
+  allAccess = [ nayuta shorekeeper hitori ];
 in
 {
-  "vpn.env.age".publicKeys = users ++ [ shorekeeper ];
-  "homepage.env.age".publicKeys = users ++ [ shorekeeper ];
-  "authelia-jwt.age".publicKeys     = users ++ [ shorekeeper ];
-  "authelia-storage.age".publicKeys = users ++ [ shorekeeper ];
-  "authelia-session.age".publicKeys = users ++ [ shorekeeper ];
-  "searxng-secret.age".publicKeys = users ++ [ shorekeeper ];
-  "anki-password.age".publicKeys = users ++ [ shorekeeper ];
+  "vpn.env.age".publicKeys = allAccess;
+  "homepage.env.age".publicKeys = allAccess; 
+  "authelia-jwt.age".publicKeys = allAccess;
+  "authelia-storage.age".publicKeys = allAccess; 
+  "authelia-session.age".publicKeys = allAccess;
+  "searxng-secret.age".publicKeys = allAccess;
+  "anki-password.age".publicKeys = allAccess;
 }
